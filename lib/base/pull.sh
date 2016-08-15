dcf2ca23c26e02998794a071af00b7b0df43694f
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2016-08-15 11:53:48 +0200

    base-local.conf now refers to base-1.0.1

--- lib/base/pull.sh
+++ lib/base/pull.sh
@@ -15,4 +15,4 @@ command="pull $CUBX_ENV_BASE_CLUSTER"
 # run
 ######################
 docker run --name cubbles_base --rm $sourcesVolume -v "/var/run/docker.sock:/var/run/docker.sock" $image $command
-docker ps | grep cubbles_base
+docker images | grep cubbles/base
