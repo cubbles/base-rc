b7b1fb54d15111bc97d69c52153560ac3d8b1fca
Hd Böhlau <hans-dieter.boehlau@incowia.com>
2016-09-08 16:51:59 +0200

    fixed backup and restore commands (did not consider latest docker-compose related changes)

--- lib/base/backup.sh
+++ lib/base/backup.sh
@@ -5,13 +5,16 @@
 # The bin/run.sh script dynamically writes the selected configuration at the beginning of this file.
 # Therefore you can use any variable defined within the etc/*.conf file here.
 
+image="cubbles/base:$CUBX_ENV_BASE_TAG"
+sourcesVolume=""
 if [ ${CUBX_ENV_BASE_CLUSTER} = "dev" ]; then
-    baseBackupFolder="$CUBX_ENV_VM_MOUNTPOINT/$CUBX_ENV_BASE_IMAGE_LOCAL_SOURCE_FOLDER/../../var"
-    baseImageFolder="$CUBX_ENV_VM_MOUNTPOINT/$CUBX_ENV_BASE_IMAGE_LOCAL_SOURCE_FOLDER"
-    docker run --rm -v "$baseBackupFolder:/backups" -v "$baseImageFolder/opt/base:/opt/base" -v "/var/run/docker.sock:/var/run/docker.sock" cubbles/base backup $CUBX_ENV_BASE_CLUSTER
-else
-    baseBackupFolder="/mnt/sda1/tmp"
-    docker run --name cubbles_base --rm -v "$baseBackupFolder:/backups" -v "/var/run/docker.sock:/var/run/docker.sock" cubbles/base:$CUBX_ENV_BASE_TAG backup $CUBX_ENV_BASE_CLUSTER
+    sourcesVolume="-v $CUBX_ENV_VM_MOUNTPOINT/$CUBX_ENV_BASE_IMAGE_LOCAL_SOURCE_FOLDER/opt/base:/opt/base"
 fi
+command="backup $CUBX_ENV_BASE_CLUSTER"
+
+######################
+# run
+######################
+docker run --name cubbles_base --rm $sourcesVolume -v "/var/run/docker.sock:/var/run/docker.sock" $image $command
 
 docker ps | grep cubbles_base
--- lib/base/restore.sh
+++ lib/base/restore.sh
@@ -5,24 +5,21 @@
 # The bin/run.sh script dynamically writes the selected configuration at the beginning of this file.
 # Therefore you can use any variable defined within the etc/*.conf file here.
 
-# Mount the volumes from the 'base.coredatastore' -container into the 'base' container.
+# We mount the volumes from the 'base.coredatastore' -container into the 'base' container.
 #  Doing so, the 'base' container gets access to the couch database folder
-coreDataStoreContainer='"'$CUBX_ENV_BASE_CLUSTER'_base.coredatastore_1"'
+coreDataStoreContainer="cubbles_base.coredatastore_1"
 
-# An example backup file ...
-# Tip: Just name your backup-file like this and you can use this script without modification.
-backupFile="base.coredatastore_volume.tar.gz"
-
+image="cubbles/base:$CUBX_ENV_BASE_TAG"
+sourcesVolume=""
 if [ ${CUBX_ENV_BASE_CLUSTER} = "dev" ]; then
-    baseBackupFolder="$CUBX_ENV_VM_MOUNTPOINT/$CUBX_ENV_BASE_IMAGE_LOCAL_SOURCE_FOLDER/../../var"
-    baseImageFolder="$CUBX_ENV_VM_MOUNTPOINT/$CUBX_ENV_BASE_IMAGE_LOCAL_SOURCE_FOLDER"
-    # run the restore
-    docker run --rm -v "$baseBackupFolder:/backups" --volumes-from=$coreDataStoreContainer -v "$baseImageFolder/opt/base:/opt/base" cubbles/base restore $CUBX_ENV_BASE_CLUSTER $backupFile
-else
-    baseBackupFolder="/var/tmp"
-    # run the restore
-    docker run --name cubbles_base --rm -v "$baseBackupFolder:/backups" --volumes-from=$coreDataStoreContainer cubbles/base:$CUBX_ENV_BASE_TAG restore $CUBX_ENV_BASE_CLUSTER $backupFile
+    sourcesVolume="-v $CUBX_ENV_VM_MOUNTPOINT/$CUBX_ENV_BASE_IMAGE_LOCAL_SOURCE_FOLDER/opt/base:/opt/base"
 fi
+command="restore $CUBX_ENV_BASE_CLUSTER $CUBX_ENV_BASE_RESTORE_FILENAME"
+
+######################
+# run
+######################
+docker run --name cubbles_base --rm $sourcesVolume -v "$CUBX_ENV_BASE_RESTORE_FOLDER:/backups" --volumes-from=$coreDataStoreContainer -v "/var/run/docker.sock:/var/run/docker.sock" $image $command
 
 # show base processes
 docker ps | grep cubbles_base
